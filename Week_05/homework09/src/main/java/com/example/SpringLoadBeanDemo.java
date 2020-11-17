95ec6711be816d50def406450fe06d459bb185f8
Reuben <793061610@qq.com>
2020-11-17 18:00:28 +0800

    to:更新第五周作业

--- Week_05/homework09/src/main/java/com/example/SpringLoadBeanDemo.java
+++ Week_05/homework09/src/main/java/com/example/SpringLoadBeanDemo.java
@@ -1,26 +1,22 @@
 package com.example;
 
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class SpringLoadBeanDemo {
-    private final static String BEAN_PATH = "spring-bean.xml";
-
+    private final static String BEAN_PATH = "classpath:spring-bean.xml";
 
     public static void main(String[] args) {
-//        Student student = loadBeanByXml( Student.class);
 //        Student student = loadBeanByAnnotation(Student.class);
+//        Student student = loadBeanByXml( Student.class);
+//        Student student = loadBeanByFile(Student.class);
         Student student = loadBeanByDefinition(Student.class);
         System.out.println(student.toString());
     }
 
-    private static <T> T loadBeanByDefinition(Class<T> clzz) {
-        return null;
-    }
-
-    private static <T> T loadBeanByXml(Class<T> clzz) {
-        return new ClassPathXmlApplicationContext(BEAN_PATH).getBean(clzz.getSimpleName().toLowerCase(), clzz);
-    }
 
     private static <T> T loadBeanByAnnotation(Class<T> clzz) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
@@ -33,5 +29,19 @@ public class SpringLoadBeanDemo {
         }
     }
 
+    private static <T> T loadBeanByXml(Class<T> clzz) {
+        return new ClassPathXmlApplicationContext(BEAN_PATH).getBean(clzz.getSimpleName().toLowerCase(), clzz);
+    }
+
+    private static <T> T loadBeanByFile(Class<T> clzz) {
+        return new FileSystemXmlApplicationContext(BEAN_PATH).getBean(clzz.getSimpleName().toLowerCase(),clzz);
+    }
+
+    private static <T> T loadBeanByDefinition(Class<T> clzz) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        RootBeanDefinition beanDefinition = new RootBeanDefinition(clzz);
+        beanFactory.registerBeanDefinition(clzz.getSimpleName().toLowerCase(),beanDefinition);
+        return beanFactory.getBean(clzz.getSimpleName().toLowerCase(),clzz);
+    }
 
 }
